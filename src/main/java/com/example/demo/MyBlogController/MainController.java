5e133e4e2e165cc6e2789250c6010dd01863e13f
aodacatwzd <wangzhoudong@bupt.edu.cn>
2019-03-28 10:04:42 +0800

    test commit

--- src/main/java/com/example/demo/MyBlogController/MainController.java
+++ src/main/java/com/example/demo/MyBlogController/MainController.java
@@ -4,8 +4,9 @@ import com.example.demo.Services.BasicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.example.demo.Services.BasicService.*;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value = "/")
@@ -13,16 +14,19 @@ public class MainController {
     @Autowired
     BasicService basicService;
 
-    @RequestMapping(value = "/home")
+    @RequestMapping(value = "/home",method = RequestMethod.GET)
     public String homePage(Model model){
-        model.addAttribute("name","aodacat");
-        basicService.create("aodwe","34334");
+        model.addAttribute("basicService",new BasicService());
         return "index/home";
     }
 
-    @RequestMapping(value = "/home/article")
-    public String articlePage(Model model){
-        return "index/articleTemplate";
+    @RequestMapping(value = "/home",method = RequestMethod.POST)
+    public String articlePage(@ModelAttribute BasicService basicService1){
+        BasicService newBasicService= new BasicService();
+        newBasicService.setName(basicService1.getName());
+        newBasicService.setContext(basicService1.getContext());
+        basicService.create(basicService1.getName(),basicService1.getContext());
+        return "index/home";
     }
 
 }
--- src/main/java/com/example/demo/Services/BasicService.java
+++ src/main/java/com/example/demo/Services/BasicService.java
@@ -8,15 +8,33 @@ import javax.annotation.Resource;
 
 @Service
 public class BasicService {
-    private final JdbcTemplate jdbcTemplate;
-
     @Autowired
-    public BasicService(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
+    JdbcTemplate jdbcTemplate = new JdbcTemplate();
+
+    public String name;
+    public String context;
+
+
+
 
     public void create(String name, String context) {
         jdbcTemplate.update("insert into article(name,context) values(?, ?)", name, context);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setContext(String context){
+        this.context=context;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getContext(){
+        return context;
     }
 }
 
