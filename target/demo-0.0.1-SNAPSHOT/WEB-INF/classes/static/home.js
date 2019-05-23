83cf1cdd0624d97a3cbb70487968528592740943
aodacatwzd <wangzhoudong@bupt.edu.cn>
2019-05-23 13:51:33 +0800

    upload the war

---
+++ target/demo-0.0.1-SNAPSHOT/WEB-INF/classes/static/home.js
@@ -0,0 +1,21 @@
+var flag = 0;
+var a;
+
+window.addEventListener("scroll", function (event) {
+    var bar = $("#bottom-bar");
+    var scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+    if (document.documentElement.scrollHeight <= document.documentElement.clientHeight + scrollTop && flag === 0) {
+        bar.css("display", "flex");
+        bar.css("animation", "showIndex 0.3s");
+        flag = 1;
+    } else if (flag === 1 && document.documentElement.scrollHeight >= document.documentElement.clientHeight + scrollTop + 100) {
+        bar.css("animation", "hideIndex 0.3s");
+        setTimeout(function () {
+            bar.css("display", "none");
+        }, 150);
+        flag = 0;
+    }
+
+});
+
+
