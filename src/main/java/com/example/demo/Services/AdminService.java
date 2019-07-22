bfde28bdb87eaa670afc2636cf9228a117165649
aodacatwzd <wangzhoudong@bupt.edu.cn>
2019-07-22 10:06:40 +0800

    test

--- src/main/java/com/example/demo/Services/AdminService.java
+++ src/main/java/com/example/demo/Services/AdminService.java
@@ -1,8 +1,9 @@
 package com.example.demo.Services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AdminService {
     private final static String cookieName = "gvsdgfiusdhgfiusdhfuw";
     private final static String cookieContent = "djfhvgkjsdfhgjfhsdkjfhaugdkjshsdfsdhfyurgfhydfwkehfjsdgkjsbyuwfdhhsdhjvgsdyugjsbvhjsdvgufgehfgjhdgjhds";
-
-
 }
